0c4e9bdc85d2670553353f25aca516750a88f31b
Shannon Ervin <sjervin07@gmail.com>
2020-04-01 23:15:16 -0500

    added mongodb, cors to server

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require ("cors");
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 // Define middleware here
+app.use(cors());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
@@ -11,13 +15,23 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+// API routes are here
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+app.use(routes);
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+  {
+    userCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
+
+
+// // Define any API routes before this runs
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
