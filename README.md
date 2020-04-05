d4340a963d2f57c692dafa2fdc8275036a0ba835
Shannon Ervin <sjervin07@gmail.com>
2020-04-05 12:02:01 -0500

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# googlebooks
+This web application uses the Google Books API and Mongo Atlas to allow users to search for a wanted book and save it for their own reference.
--- google-books/src/App.js
+++ google-books/src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import save from '../src/pages/save';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
 
+import NoMatch from "./pages/NoMatch";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 
-function App() {
-  return (
-    <save></save>
-  );
-}
+import "./App.css";
+
+class App extends Component {
+ 
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav/>
+          <Header />
+          <Wrapper>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+            <Route exact path="/noMatch" component={NoMatch} />
+          </Wrapper>
+        </div>
+      </Router>
+    )
+  };
+};
 
 export default App;
